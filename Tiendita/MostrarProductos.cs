8fbd2d33174926f3189ee427c6b369dade35aac3
Lizbeth Guadalupe Lopez Abundiz <lizbethlopezabundiz@gmail.com>
2024-09-16 17:46:19 -0600

    Se agrego la funcion de modificar

--- Tiendita/MostrarProductos.cs
+++ Tiendita/MostrarProductos.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,36 @@ namespace Tiendita
     public partial class MostrarProductos : Form
     {
         ManejadorMostrar mm;
+        int fila = 0, columna = 0;
+        public static int id = 0, precio = 0;
+        public static string nombre = "",descripcion="";
+
+        private void dtgvProductos_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            fila = e.RowIndex; columna = e.ColumnIndex;
+
+            switch (columna)
+            {
+                case 4:
+                    {
+ 
+                    }
+                    break;
+                case 5:
+                    {
+                        id = int.Parse(dtgvProductos.Rows[fila].Cells[0].Value.ToString());
+                        nombre = dtgvProductos.Rows[fila].Cells[1].Value.ToString();
+                        descripcion = dtgvProductos.Rows[fila].Cells[2].Value.ToString();
+                        precio = int.Parse(dtgvProductos.Rows[fila].Cells[3].Value.ToString());
+
+                        FrmDatosProductos dl = new FrmDatosProductos();
+                        dl.ShowDialog();
+                        dtgvProductos.Visible = false;
+                    }
+                    break;
+            }
+        }            
+      
         public MostrarProductos()
         {
             InitializeComponent();
@@ -28,6 +59,7 @@ namespace Tiendita
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
+            id = 0; precio = 0; nombre = ""; descripcion = "";
             dtgvProductos.Visible = false;
             FrmDatosProductos dp = new FrmDatosProductos();
             dp.ShowDialog();
