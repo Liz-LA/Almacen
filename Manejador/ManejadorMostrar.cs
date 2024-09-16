7eef22f7a308ac0e43a68fae2b6d8a54e346c72f
Lizbeth Guadalupe Lopez Abundiz <lizbethlopezabundiz@gmail.com>
2024-09-16 15:45:05 -0600

    Muestra los datos de la tabla Producto

---
+++ Manejador/ManejadorMostrar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AccesoDatos;
+
+namespace Manejador
+{
+    public class ManejadorMostrar
+    {
+        Funciones f = new Funciones();
+        public void Mostrar(DataGridView tabla, string filtro)
+        {
+            tabla.Columns.Clear();
+            tabla.DataSource = f.Mostrar($"select * from Producto where nombre like'%{filtro}%'", "Producto").Tables[0];
+            tabla.Columns.Insert(4, Boton("Borrar", Color.Red));
+            tabla.Columns.Insert(5, Boton("Modificar", Color.Green));
+            tabla.AutoResizeColumns();
+            tabla.AutoResizeRows();
+        }
+        DataGridViewButtonColumn Boton(string t, Color co)
+        {
+            DataGridViewButtonColumn bo = new DataGridViewButtonColumn();
+            bo.Text = t;
+            bo.UseColumnTextForButtonValue = true;
+            bo.FlatStyle = FlatStyle.Popup;
+            bo.DefaultCellStyle.BackColor = co;
+            bo.DefaultCellStyle.ForeColor = Color.White;
+            return bo;
+        }
+    }
+}
--- Tiendita/Form1.cs
+++ Tiendita/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Manejador;
 
 namespace Tiendita
 {
     public partial class Form1 : Form
     {
+        ManejadorMostrar mm;
         public Form1()
         {
             InitializeComponent();
+            mm = new ManejadorMostrar();
+        }
+
+        private void txtProducto_TextChanged(object sender, EventArgs e)
+        {
+            mm.Mostrar(dtgvProductos,txtProducto.Text);
         }
     }
 }
